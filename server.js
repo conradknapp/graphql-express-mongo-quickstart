e098af15fb0b7a1711962f924622ed61fb528ea2
Conrad Knapp <conrad.knapp@bc.edu>
2018-01-28 03:27:55 -0500

    Adds 'engines' to package.json and process.env.PORT to server.js for more convenient deployment

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 // express-graphql connects GraphQL with Express and graphql-tools  creates our schema
-import expressGraphQL from 'express-graphql';
-import { makeExecutableSchema } from 'graphql-tools';
+import expressGraphQL from "express-graphql";
+import { makeExecutableSchema } from "graphql-tools";
 // Note: you can also use { graphqlExpress } from 'apollo-server-express' instead of express-graphql
 
 /* Add our files needed to make a GraphQL schema: 1) type defs, and 2) resolvers
 Type definitions (for GraphQL to check against our data) and resolvers (queries/mutations of our data, which will reach out to our database) */
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
 // Brings together the type defs and resolvers and turns them into a usable schema
 const schema = makeExecutableSchema({
@@ -20,13 +20,16 @@ const schema = makeExecutableSchema({
 });
 
 // Establish connection with Mongo
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect("mongodb://localhost/test");
 mongoose.connection
-  .once('open', () => console.log('Connected to Mongo'))
-  .on('error', error => console.error(`Error connecting to Mongo ${error}`));
+  .once("open", () => console.log("Connected to Mongo"))
+  .on("error", error => console.error(`Error connecting to Mongo ${error}`));
 
 // Define our model for Mongo (here it is only a couple of data fields, 'name' and 'lives')
-const Cat = mongoose.model('Cat', { name: String, lives: { type: Number, default: 0 } });
+const Cat = mongoose.model("Cat", {
+  name: String,
+  lives: { type: Number, default: 0 }
+});
 
 const app = express();
 
@@ -38,14 +41,17 @@ app.use(bodyParser.json());
 2) It provides context for our database model where we can pass in all our models (if we had more than one)
 3) Creates GraphiQL app at /graphql (for interacting with our data) */
 
-app.use('/graphql', expressGraphQL({ 
-  schema, 
-  context: { Cat },
-  graphiql: true 
-}));
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    context: { Cat },
+    graphiql: true
+  })
+);
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-  console.log(`Listening at ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
